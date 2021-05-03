5be17640d6ff056c46991bea62a152ca84580b09
glaulher <glaulher.developer@gmail.com>
2021-05-03 20:30:01 -0300

    update migration

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import { router } from './routes';
+import 'reflect-metadata';
+import './database';
 
 const app = express();
+app.use(express.json());
+app.use(router);
 
-export { app };
+// eslint-disable-next-line no-console
+app.listen(3333, () => console.log('Server is run :D'));
